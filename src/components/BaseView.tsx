648a477649cf0c8f7fdc1dae6a6915cc4f058f63
Dániel Sipos <dsipos@szintezis-net.hu>
2020-02-09 19:25:05 +0100

    Fixed safe area view issues

--- src/components/BaseView.tsx
+++ src/components/BaseView.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform, SafeAreaView } from 'react-native';
 import { Colors } from '../constants';
 
 export default function BaseView(props) {
     return (
-        <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? 'padding' : null} style={{ flex: 1, backgroundColor: Colors.white }}>
-            {props.children}
-        </KeyboardAvoidingView>
+        <SafeAreaView style={styles.areaView}>
+            <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? 'padding' : null} style={{ flex: 1, backgroundColor: Colors.white }}>
+                {props.children}
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    areaView: {
+        flex: 1,
+        backgroundColor: Colors.primary,
+    },
+});
