da38632c781d609adb73833e5cf77036e9bef369
Dániel Sipos <dsipos@szintezis-net.hu>
2020-02-09 22:28:19 +0100

    Add close button to headerImage

--- src/components/HeaderImage.tsx
+++ src/components/HeaderImage.tsx
@@ -3,14 +3,23 @@ import { Styles, Colors } from '../constants';
 import { View, Text, Image, StyleSheet, ImageSourcePropType } from 'react-native';
 import { useColorScheme } from 'react-native-appearance';
 import environment from '../config/environment';
+import { MaterialIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function HeaderImage(props: { title: string, imageSource: ImageSourcePropType }) {
+export default function HeaderImage(props: { title: string, imageSource: ImageSourcePropType, onClose?: () => void }) {
   const isDarkTheme = environment.dark_theme === null ? useColorScheme() === 'dark' : environment.dark_theme;
 
   return (
     <View style={styles.baseContainer}>
       <Image source={props.imageSource} style={[styles.image]} />
       <Text style={[styles.titleText, isDarkTheme ? styles.titleDarkText : styles.titleLightText]}>{props.title}</Text>
+      {props.onClose && (
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity activeOpacity={Styles.activeOpacity} onPress={props.onClose}>
+            <MaterialIcons style={[styles.icon, isDarkTheme ? styles.titleDarkText : styles.titleLightText]} name='close' />
+            </TouchableOpacity>
+            </View>
+      )}
     </View>
   );
 }
@@ -20,6 +29,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginBottom: 20,
     alignItems: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  icon: {
+    fontSize: 26,
   },
   image: {
     height: 50,
