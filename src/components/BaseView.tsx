189c906f4c63791d6e08e3aefda51ff4a30357be
Dániel Sipos <siposdani87@gmail.com>
2020-02-11 20:13:58 +0100

    Add optional barStyle to BaseView

--- src/components/BaseView.tsx
+++ src/components/BaseView.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { StyleSheet, KeyboardAvoidingView, Platform, StatusBar, StatusBarStyle, View } from 'react-native';
 import { Colors } from '../constants';
 
-export default function BaseView(props: {barStyle: StatusBarStyle, backgroundColor?: any, children: any}) {
+export default function BaseView(props: {barStyle?: StatusBarStyle, backgroundColor?: any, children: any}) {
     const backgroundColor = props.backgroundColor || 'transparent';
     const translucent = backgroundColor === 'transparent';
     return (
         <View style={[styles.areaView, {backgroundColor: backgroundColor }]}>
-            <StatusBar barStyle={props.barStyle} backgroundColor={backgroundColor} translucent={translucent} />
+            <StatusBar barStyle={props.barStyle || 'light-content'} backgroundColor={backgroundColor} translucent={translucent} />
             
             <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? 'padding' : null} style={{ flex: 1, backgroundColor: Colors.white }}>
                 {props.children}
