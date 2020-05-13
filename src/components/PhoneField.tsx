f493db5c12d47f48ca046b31a6c1c45bc1ae8af4
Dániel Sipos <dsipos@szintezis-net.hu>
2020-05-13 14:55:16 +0200

    Use TextInputProps in field props definition

--- src/components/PhoneField.tsx
+++ src/components/PhoneField.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import TextField from './TextField';
+import { TextInputProps } from 'react-native';
 
-export default function PhoneField(props: { value: any, label: string, error: any, onValueChange: (value: any) => void, required?: boolean, disabled?: boolean, style?: any}) {
+export default function PhoneField(props: { value: any, label: string, error: any, onValueChange: (value: any) => void, required?: boolean, disabled?: boolean, style?: any} & TextInputProps) {
   return (
     <TextField {...props} value={(props.value || '').toString()} keyboardType='phone-pad' />
   );
