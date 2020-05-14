f1685c44b3798fa8cce49ca5130dd41d90ba2ac3
Dániel Sipos <dsipos@szintezis-net.hu>
2020-05-14 17:36:29 +0200

    Fixed style of FileField

--- src/components/TextAreaField.tsx
+++ src/components/TextAreaField.tsx
@@ -5,7 +5,8 @@ import { TextInputProps } from 'react-native';
 export default function TextAreaField(props: { value: any, label: string, error: any, onValueChange: (value: any) => void, required?: boolean, disabled?: boolean, style?: any} & TextInputProps) {
   const numberOfLines = props.numberOfLines || 4;
   const style = {
-    height: 36 * numberOfLines,
+    height: 20 * numberOfLines + 16,
+    textAlignVertical: 'top',
     ...props.style,
   };
 
