768d1a1f4844ae283eec14a34c715bee0760bbda
Dániel Sipos <siposdani87@hotmail.com>
2020-05-22 21:17:45 +0200

    Fixed style of iconButton

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,46 +3,41 @@ import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { Colors, Styles } from '../constants';
 
-export default function IconButton(props: { onPress: () => void, textColor?: string, color?: string, borderColor?: string, title?: string, source?: any, iconName?: string, iconType?: string, style?: any}) {
+export default function IconButton(props: { onPress: () => void, textColor?: string, color?: string, borderColor?: string, title?: string, source?: any, iconName?: string, iconType?: string, style?: any }) {
     const backgroundColor = props.color || Colors.primary;
     return (
-        <View style={props.style}>
-            <TouchableOpacity activeOpacity={Styles.activeOpacity} onPress={props.onPress}>
-                <View style={[styles.buttonContainer, { backgroundColor, borderColor: props.borderColor || backgroundColor }]}>
-                    {props.source && (
-                        <Image style={styles.image} source={props.source} />
-                    )}
-                    {props.iconName && !props.iconType && (
-                        <MaterialIcons style={[styles.icon, { color: props.textColor || Colors.white }]} name={props.iconName} />
-                    )}
-                    {props.iconName && props.iconType === 'Community' && (
-                        <MaterialCommunityIcons style={[styles.icon, { color: props.textColor || Colors.white }]} name={props.iconName} />
-                    )}
-                </View>
-            </TouchableOpacity>
-        </View>
+        <TouchableOpacity activeOpacity={Styles.activeOpacity} onPress={props.onPress}>
+            <View style={[styles.buttonContainer, { backgroundColor, borderColor: props.borderColor || backgroundColor }, props.style]}>
+                {props.source && (
+                    <Image style={styles.image} source={props.source} />
+                )}
+                {props.iconName && !props.iconType && (
+                    <MaterialIcons style={[styles.icon, { color: props.textColor || Colors.white }]} name={props.iconName} />
+                )}
+                {props.iconName && props.iconType === 'Community' && (
+                    <MaterialCommunityIcons style={[styles.icon, { color: props.textColor || Colors.white }]} name={props.iconName} />
+                )}
+            </View>
+        </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
     buttonContainer: {
-        minHeight: 36,
         borderRadius: 50,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
         borderWidth: 1,
-        minWidth: 36,
         margin: 5,
+        padding: 8,
         ...Styles.lightShadow,
     },
     image: {
         width: 20,
         height: 20,
-        marginRight: 10,
     },
     icon: {
         fontSize: 26,
-        padding: 12,
     },
 });
