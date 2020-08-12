d379d90482b8699947b9ae40d90e12e790bbf8b2
Dániel Sipos <siposdani87@hotmail.com>
2020-08-12 13:08:21 +0200

    Fixed richText property on TextAreaField

--- src/components/TextAreaField.tsx
+++ src/components/TextAreaField.tsx
@@ -4,68 +4,64 @@ import ErrorField from './ErrorField';
 import Label from './Label';
 import { TextInputProps, View, StyleSheet } from 'react-native';
 import { RichToolbar, RichEditor } from 'react-native-pell-rich-editor';
-// import CNRichTextEditor, { CNToolbar, convertToHtmlString, convertToObject, getDefaultStyles, getInitialObject } from 'react-native-cn-richtext-editor';
-import { Colors } from '../constants';
+import { Colors, Styles } from '../constants';
 import useBaseField from './useBaseField';
 import { useColorScheme } from 'react-native-appearance';
 import environment from '../config/environment';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TextAreaField(props: { value: any, label: string, error: any, onValueChange: (value: any) => void, required?: boolean, disabled?: boolean, richText?: boolean, style?: any, containerStyle?: any } & TextInputProps) {
   const [value, setValue] = useState(props.value);
-  // const [initValue, setInitValue] = useState(convert(value));
-  // const [selectedTag, setSelectedTag] = useState('body');
-  // const [selectedStyles, setSelectedStyles] = useState([]);
   const [error, onErrorChange] = useBaseField(props);
   const hasError = error || (props.required && (!value || value && value.length === 0));
   const isDarkTheme = environment.dark_theme === null ? useColorScheme() === 'dark' : environment.dark_theme;
   const [editor, setEditor] = useState(null);
   const numberOfLines = props.numberOfLines || 5;
   const style = {
-    height: 20 * numberOfLines + (props.richText ? 65 : 16),
+    [props.richText ? 'minHeight' : 'height']: 20 * numberOfLines + (props.richText ? 65 : 16),
+    maxHeight: 300,
     textAlignVertical: 'top',
     ...props.style,
   };
-  // const defaultStyles = getDefaultStyles();
 
   useEffect(() => {
     setValue(props.value);
   }, [props.value]);
 
-  /* useEffect(() => {
-    setInitValue(convert(value));
-  }, [value]);
-
-  function convert(v){
-    if (v){
-      //return convertToObject((v || '').replace('br', 'br /'));
-    }
-    return getInitialObject();
-  } */
-
   async function onValueChange() {
     const v = await editor.getContentHtml();
-    console.log('v', v);
-    // const v = convertToHtmlString(o);
     onErrorChange();
     props.onValueChange(v);
-    // setInitValue(o);
     // setValue(v);
   }
 
-  /* function onStyleKeyPress(toolType) {
-    editor.applyToolbar(toolType);
-  }
-
-  function onSelectedTagChanged(tag) {
-    setSelectedTag(tag);
-  }
-
-  function onSelectedStyleChanged(s) {
-    setSelectedStyles(s);
-  } */
-
   function onHeightChange() {
-    // console.log('height', h)
+    // console.log('onHeightChange', h);
+  }
+
+  function editorInitializedCallback() {
+    // console.log('editorInitializedCallback');
+  }
+
+  function getEditor() {
+    return editor;
+  }
+
+  function iconMap() {
+    function getColor(_selected) {
+      if (true) {
+        return isDarkTheme ? Colors.primaryBright : Colors.primary;
+      }
+      // return isDarkTheme ? Colors.white : Colors.black;
+    }
+
+    return {
+      bold: ({selected}) => (<MaterialIcons name='format-bold' size={22} color={getColor(selected)} />),
+      italic: ({selected}) => (<MaterialIcons name='format-italic' size={22} color={getColor(selected)} />),
+      underline: ({selected}) => (<MaterialIcons name='format-underlined' size={22} color={getColor(selected)} />),
+      unorderedList: ({selected}) => (<MaterialIcons name='format-list-bulleted' size={22} color={getColor(selected)} />),
+      orderedList: ({selected}) => (<MaterialIcons name='format-list-numbered' size={22} color={getColor(selected)} />),
+    };
   }
 
   function _getTextInputStyle() {
@@ -88,81 +84,11 @@ export default function TextAreaField(props: { value: any, label: string, error:
     return (
       <View style={[styles.container, props.containerStyle]}>
         <Label label={props.label} required={props.required} disabled={props.disabled} />
-        {/* <View style={[styles.textInput, style, _getTextInputStyle()]}>
-          {!!editor && (
-            <CNToolbar
-              style={{
-                height: 35,
-              }}
-              iconSetContainerStyle={{
-                flexGrow: 1,
-                justifyContent: 'space-evenly',
-                alignItems: 'center',
-              }}
-              size={30}
-              iconSet={[
-                {
-                  type: 'tool',
-                  iconArray: [{
-                    toolTypeText: 'italic',
-                    buttonTypes: 'style',
-                    iconComponent: <Text style={styles.toolbarButton}>italic</Text>,
-                  }],
-                },
-                {
-                  type: 'tool',
-                  iconArray: [{
-                    toolTypeText: 'bold',
-                    buttonTypes: 'style',
-                    iconComponent: <Text style={styles.toolbarButton}>bold</Text>,
-                  }],
-                },
-                {
-                  type: 'seperator',
-                },
-                {
-                  type: 'tool',
-                  iconArray: [
-                    {
-                      toolTypeText: 'body',
-                      buttonTypes: 'tag',
-                      iconComponent: <Text style={styles.toolbarButton}>body</Text>,
-                    },
-                  ],
-                },
-                {
-                  type: 'tool',
-                  iconArray: [
-                    {
-                      toolTypeText: 'ul',
-                      buttonTypes: 'tag',
-                      iconComponent: <Text style={styles.toolbarButton}>ul</Text>,
-                    },
-                  ],
-                },
-                {
-                  type: 'tool',
-                  iconArray: [
-                    {
-                      toolTypeText: 'ol',
-                      buttonTypes: 'tag',
-                      iconComponent: <Text style={styles.toolbarButton}>ol</Text>,
-                    },
-                  ],
-                },
-              ]}
-              selectedTag={selectedTag}
-              selectedStyles={selectedStyles}
-              onStyleKeyPress={onStyleKeyPress}
-            />
-          )}
-          <CNRichTextEditor ref={(r) => setEditor(r)} placeholder='' textInputStyle={[styles.textInput, style, _getTextInputStyle(), {borderWidth: 0}]} onSelectedTagChanged={onSelectedTagChanged} onSelectedStyleChanged={onSelectedStyleChanged} value={initValue} style={{ backgroundColor }} styleList={defaultStyles} onValueChanged={onValueChange} />
-        </View> */}
+        {!!editor && (
+          <RichToolbar getEditor={getEditor} style={styles.toolbar} actions={['bold', 'italic', 'underline', 'unorderedList', 'orderedList']} iconMap={iconMap()} disabled={props.disabled} />
+        )}
         <View style={[styles.textInput, style, _getTextInputStyle()]}>
-          {!!editor && (
-          <RichToolbar getEditor={() => editor} actions={['bold', 'italic', 'unorderedList', 'orderedList']} disabled={props.disabled} />
-          )}
-          <RichEditor ref={(r) => setEditor(r)} initialContentHTML={props.value} editorInitializedCallback={() => null} onHeightChange={onHeightChange} onChange={onValueChange} editorStyle={{backgroundColor, color}} />
+          <RichEditor ref={(r) => setEditor(r)} initialContentHTML={props.value} editorInitializedCallback={editorInitializedCallback} onHeightChange={onHeightChange} onChange={onValueChange} editorStyle={{backgroundColor, color}} />
         </View>
         <ErrorField error={error} disabled={props.disabled} />
       </View>
@@ -179,12 +105,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   textInput: {
-    // fontFamily: Styles.fontFamilyBody,
-    // fontSize: 16,
-    // height: 36,
+    fontFamily: Styles.fontFamilyBody,
+    fontSize: 16,
     borderRadius: 3,
     borderWidth: 1,
-    // paddingHorizontal: 10,
+    paddingHorizontal: 0,
+    marginVertical: 0,
   },
   defaultLightTextInput: {
     color: Colors.contentDefaultLight,
@@ -220,27 +146,10 @@ const styles = StyleSheet.create({
     color: Colors.contentDisabledDark,
     borderColor: Colors.errorDisabledDark,
   },
-  main: {
-    flex: 1,
-    marginTop: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-    paddingBottom: 1,
-    alignItems: 'stretch',
-  },
-  toolbarButton: {
-    fontSize: 20,
-    width: 28,
-    height: 28,
-    textAlign: 'center',
-  },
-  boldButton: {
-    fontWeight: 'bold',
-  },
-  underlineButton: {
-    textDecorationLine: 'underline',
-  },
-  lineThroughButton: {
-    textDecorationLine: 'line-through',
+  toolbar: {
+    height: 40,
+    backgroundColor: 'transparent',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
   },
 });
