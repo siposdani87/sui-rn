3a7f0c2b08ce233fe0936868bf9422812f93609b
Dániel Sipos <dsipos@szintezis-net.hu>
2020-08-14 11:31:11 +0200

    Set HTTP_403 status

--- src/constants/ActionTypes.ts
+++ src/constants/ActionTypes.ts
@@ -3,3 +3,4 @@ export const NOTIFICATION = 'Notification/NOTIFICATION';
 export const HTTP_REQUEST = 'Http/REQUEST';
 export const HTTP_RESPONSE = 'Http/RESPONSE';
 export const HTTP_401 = 'Http/STATUS_401';
+export const HTTP_403 = 'Http/STATUS_403';
