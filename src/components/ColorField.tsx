ea4331a9da236dce415ec6a70f647c243f159215
Dániel Sipos <siposdani87@hotmail.com>
2020-10-02 20:42:49 +0200

    Create ColorField and SliderField

---
+++ src/components/ColorField.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import ErrorField from './ErrorField';
+import Label from './Label';
+import { View, StyleSheet } from 'react-native';
+import { Colors, Styles } from '../constants';
+import useBaseField from './useBaseField';
+import { useColorScheme } from 'react-native-appearance';
+import environment from '../config/environment';
+import { TriangleColorPicker } from 'react-native-color-picker';
+
+export default function ColorField(props: { value: any, label: string, error: any, onValueChange: (value: any) => void, required?: boolean, disabled?: boolean, style?: any, containerStyle?: any }) {
+  const [value, setValue] = useState(props.value);
+  const [error, onErrorChange] = useBaseField(props);
+  const hasError = error || (props.required && (!value || value && value.length === 0));
+  const isDarkTheme = environment.dark_theme === null ? useColorScheme() === 'dark' : environment.dark_theme;
+
+  useEffect(() => {
+    setValue(props.value);
+  }, [props.value]);
+
+  function onValueChange(v) {
+    onErrorChange();
+    props.onValueChange(v);
+    setValue(v);
+  }
+
+  function _getTextInputStyle() {
+    if (hasError) {
+      if (props.disabled) {
+        return isDarkTheme ? styles.hasErrorDisabledDark : styles.hasErrorDisabledLight;
+      }
+      return isDarkTheme ? styles.hasErrorDefaultDark : styles.hasErrorDefaultLight;
+    }
+    if (props.disabled) {
+      return isDarkTheme ? styles.disabledDarkTextInput : styles.disabledLightTextInput;
+    }
+    return isDarkTheme ? styles.defaultDarkTextInput : styles.defaultLightTextInput;
+  }
+
+  return (
+    <View style={[styles.container, props.containerStyle]}>
+      <Label label={props.label} required={props.required} disabled={props.disabled} />
+      <TriangleColorPicker style={[{flex: 1}, props.style, _getTextInputStyle()]} oldColor={props.value} onColorSelected={onValueChange} />
+      <ErrorField error={error} disabled={props.disabled} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  textInput: {
+    fontFamily: Styles.fontFamilyBody,
+    fontSize: 16,
+    height: 36,
+    borderRadius: 3,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  defaultLightTextInput: {
+    color: Colors.contentDefaultLight,
+    borderColor: Colors.inputDefaultLight,
+  },
+  defaultDarkTextInput: {
+    color: Colors.contentDefaultDark,
+    borderColor: Colors.inputDefaultDark,
+  },
+  disabledLightTextInput: {
+    color: Colors.contentDisabledLight,
+    borderColor: Colors.inputDisabledLight,
+    borderStyle: 'dotted',
+  },
+  disabledDarkTextInput: {
+    color: Colors.contentDisabledDark,
+    borderColor: Colors.inputDisabledDark,
+    borderStyle: 'dotted',
+  },
+  hasErrorDefaultLight: {
+    color: Colors.contentDefaultLight,
+    borderColor: Colors.errorDefaultLight,
+  },
+  hasErrorDefaultDark: {
+    color: Colors.contentDefaultDark,
+    borderColor: Colors.errorDefaultDark,
+  },
+  hasErrorDisabledLight: {
+    color: Colors.contentDisabledLight,
+    borderColor: Colors.errorDisabledLight,
+  },
+  hasErrorDisabledDark: {
+    color: Colors.contentDisabledDark,
+    borderColor: Colors.errorDisabledDark,
+  },
+});
---
+++ src/components/SliderField.tsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import ErrorField from './ErrorField';
+import Label from './Label';
+import { View, StyleSheet } from 'react-native';
+import { Colors, Styles } from '../constants';
+import useBaseField from './useBaseField';
+import { useColorScheme } from 'react-native-appearance';
+import environment from '../config/environment';
+import Slider from '@react-native-community/slider';
+
+export default function SliderField(props: { value: any, label: string, error: any, onValueChange: (value: any) => void, required?: boolean, disabled?: boolean, style?: any, containerStyle?: any, minimumValue?: number, maximumValue?: number, step?: number }) {
+  const [value, setValue] = useState(props.value);
+  const [error, onErrorChange] = useBaseField(props);
+  const hasError = error || (props.required && (!value || value && value.length === 0));
+  const isDarkTheme = environment.dark_theme === null ? useColorScheme() === 'dark' : environment.dark_theme;
+
+
+  useEffect(() => {
+    setValue(props.value);
+  }, [props.value]);
+
+  function onValueChange(v) {
+    onErrorChange();
+    props.onValueChange(v);
+    setValue(v);
+  }
+
+  function _getTextInputStyle() {
+    if (hasError) {
+      if (props.disabled) {
+        return isDarkTheme ? styles.hasErrorDisabledDark : styles.hasErrorDisabledLight;
+      }
+      return isDarkTheme ? styles.hasErrorDefaultDark : styles.hasErrorDefaultLight;
+    }
+    if (props.disabled) {
+      return isDarkTheme ? styles.disabledDarkTextInput : styles.disabledLightTextInput;
+    }
+    return isDarkTheme ? styles.defaultDarkTextInput : styles.defaultLightTextInput;
+  }
+
+  return (
+    <View style={[styles.container, props.containerStyle]}>
+      <Label label={props.label} required={props.required} disabled={props.disabled} />
+      <Slider style={[{flex: 1, height: 40}, props.style, _getTextInputStyle()]} value={props.value} onValueChange={onValueChange} step={props.step} minimumValue={props.minimumValue} maximumValue={props.minimumValue} minimumTrackTintColor={Colors.grey} maximumTrackTintColor={Colors.grey} disabled={props.disabled} />
+      <ErrorField error={error} disabled={props.disabled} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  textInput: {
+    fontFamily: Styles.fontFamilyBody,
+    fontSize: 16,
+    height: 36,
+    borderRadius: 3,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  defaultLightTextInput: {
+    color: Colors.contentDefaultLight,
+    borderColor: Colors.inputDefaultLight,
+  },
+  defaultDarkTextInput: {
+    color: Colors.contentDefaultDark,
+    borderColor: Colors.inputDefaultDark,
+  },
+  disabledLightTextInput: {
+    color: Colors.contentDisabledLight,
+    borderColor: Colors.inputDisabledLight,
+    borderStyle: 'dotted',
+  },
+  disabledDarkTextInput: {
+    color: Colors.contentDisabledDark,
+    borderColor: Colors.inputDisabledDark,
+    borderStyle: 'dotted',
+  },
+  hasErrorDefaultLight: {
+    color: Colors.contentDefaultLight,
+    borderColor: Colors.errorDefaultLight,
+  },
+  hasErrorDefaultDark: {
+    color: Colors.contentDefaultDark,
+    borderColor: Colors.errorDefaultDark,
+  },
+  hasErrorDisabledLight: {
+    color: Colors.contentDisabledLight,
+    borderColor: Colors.errorDisabledLight,
+  },
+  hasErrorDisabledDark: {
+    color: Colors.contentDisabledDark,
+    borderColor: Colors.errorDisabledDark,
+  },
+});
