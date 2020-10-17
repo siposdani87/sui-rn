6e46894ec6a4ef367cee4c8e1721149998dadb67
Dániel Sipos <siposdani87@hotmail.com>
2020-10-17 15:35:19 +0200

    Improved TextAreaField

--- src/components/TextAreaField.tsx
+++ src/components/TextAreaField.tsx
@@ -3,7 +3,7 @@ import TextField from './TextField';
 import ErrorField from './ErrorField';
 import Label from './Label';
 import { TextInputProps, View, StyleSheet } from 'react-native';
-import { RichToolbar, RichEditor } from 'react-native-pell-rich-editor';
+import RichTextEditor from 'expo-rich-text-editor/src/RichTextEditor';
 import { Colors, Styles } from '../constants';
 import useBaseField from './useBaseField';
 import { useColorScheme } from 'react-native-appearance';
@@ -15,10 +15,9 @@ export default function TextAreaField(props: { value: any, label: string, error:
   const [error, onErrorChange] = useBaseField(props);
   const hasError = error || (props.required && (!value || value && value.length === 0));
   const isDarkTheme = environment.dark_theme === null ? useColorScheme() === 'dark' : environment.dark_theme;
-  const [editor, setEditor] = useState(null);
   const numberOfLines = props.numberOfLines || 5;
   const style = {
-    [props.richText ? 'minHeight' : 'height']: 20 * numberOfLines + (props.richText ? 65 : 16),
+    height: 20 * numberOfLines + 16,
     maxHeight: 300,
     textAlignVertical: 'top',
     ...props.style,
@@ -28,28 +27,15 @@ export default function TextAreaField(props: { value: any, label: string, error:
     setValue(props.value);
   }, [props.value]);
 
-  async function onValueChange() {
-    const v = await editor.getContentHtml();
-    // console.log('onValueChange', v);
-    onErrorChange();
-    props.onValueChange(v);
+  function onValueChange(v) {
+    console.log('onValueChange', v);
+    // onErrorChange();
+    //props.onValueChange(v);
     // setValue(v);
   }
-  // console.log('value', value);
 
-  function onHeightChange() {
-    // console.log('onHeightChange', h);
-  }
 
-  function editorInitializedCallback() {
-    // console.log('editorInitializedCallback');
-  }
-
-  function getEditor() {
-    return editor;
-  }
-
-  function iconMap() {
+  function getActionMap() {
     function getColor(_selected) {
       if (true) {
         return isDarkTheme ? Colors.primaryBright : Colors.primary;
@@ -81,17 +67,13 @@ export default function TextAreaField(props: { value: any, label: string, error:
 
   const backgroundColor = isDarkTheme ? Colors.black : Colors.white;
   const color = isDarkTheme ? Colors.white : Colors.black;
+  const editorStyle = [styles.textInput, style, _getTextInputStyle(), {backgroundColor, color}];
 
   if (props.richText) {
     return (
       <View style={[styles.container, props.containerStyle]}>
         <Label label={props.label} required={props.required} disabled={props.disabled} />
-        {!!editor && (
-          <RichToolbar getEditor={getEditor} style={styles.toolbar} actions={['bold', 'italic', 'underline', 'unorderedList', 'orderedList']} iconMap={iconMap()} disabled={props.disabled} />
-        )}
-        <View style={[styles.textInput, style, _getTextInputStyle()]}>
-          <RichEditor ref={(r) => setEditor(r)} initialContentHTML={value} editorInitializedCallback={editorInitializedCallback} onHeightChange={onHeightChange} onChange={onValueChange} editorStyle={{backgroundColor, color}} />
-        </View>
+        <RichTextEditor minHeight={style.height} value={value} onValueChange={onValueChange} actionMap={getActionMap()} toolbarStyle={styles.toolbar} editorStyle={{}} />
         <ErrorField error={error} disabled={props.disabled} />
       </View>
     );
@@ -150,9 +132,5 @@ const styles = StyleSheet.create({
     borderColor: Colors.errorDisabledDark,
   },
   toolbar: {
-    height: 40,
-    backgroundColor: 'transparent',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
   },
 });
