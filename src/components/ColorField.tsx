9a6523915903c745d18072bbeeaa3e6065a376e7
Dániel Sipos <siposdani87@hotmail.com>
2020-10-25 17:31:54 +0100

    Improved color picker

--- src/components/ColorField.tsx
+++ src/components/ColorField.tsx
@@ -1,31 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ErrorField from './ErrorField';
+import SUI from 'sui-js';
 import Label from './Label';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Colors, Styles } from '../constants';
 import useBaseField from './useBaseField';
 import { useColorScheme } from 'react-native-appearance';
 import environment from '../config/environment';
-import { ColorPicker, toHsv } from 'react-native-color-picker';
+import HsvColorPicker from 'react-native-hsv-color-picker';
 import Dialog from './Dialog';
+import Button from './Button';
 
-export default function ColorField(props: { value: any, onValueChange: (value: any) => void, label?: string, error?: any, required?: boolean, disabled?: boolean, containerStyle?: any, style?: any }) {
+export default function ColorField(props: { value: any, onValueChange: (value: any) => void, okText: string, label?: string, error?: any, required?: boolean, disabled?: boolean, containerStyle?: any, style?: any }) {
   const [value, setValue] = useState(props.value);
-  const [color, setColor] = useState(toHsv(props.value));
+  const [color, setColor] = useState(SUI.HEXToHSV(props.value));
+  const [hue, setHue] = useState(0);
+  const [sat, setSat] = useState(0);
+  const [val, setVal] = useState(1);
   const [error, onErrorChange] = useBaseField(props);
   const [show, setShow] = useState(false);
+  const colorPickerRef = useRef(null);
   const hasError = error || (props.required && (!value || value && value.length === 0));
   const isDarkTheme = environment.dark_theme === null ? useColorScheme() === 'dark' : environment.dark_theme;
 
   useEffect(() => {
     setValue(props.value);
-    setColor(toHsv(props.value));
+    setColor(SUI.HEXToHSV(props.value));
   }, [props.value]);
 
   function onValueChange(v) {
     onErrorChange();
     setValue(v);
-    setColor(toHsv(v));
+    setColor(SUI.HEXToHSV(v));
     props.onValueChange(v);
   }
 
@@ -42,21 +48,49 @@ export default function ColorField(props: { value: any, onValueChange: (value: a
     return isDarkTheme ? styles.defaultDarkTextInput : styles.defaultLightTextInput;
   }
 
-  function showColorPicker(){
-    // setShow(true);
+  function showColorPicker() {
+    const [h, s, v] = color;
+    setHue(h);
+    setSat(s);
+    setVal(v);
+    setShow(true);
   }
 
-  function hideColorPicker(){
+  function hideColorPicker() {
     setShow(false);
+  }
+
+  function onSatValPickerChange(c) {
+    setSat(c.saturation);
+    setVal(c.value);
+  }
+
+  function onHuePickerChange(c) {
+    setHue(c.hue);
+  }
+
+  function selectColor() {
+    hideColorPicker();
+    const hexColor = colorPickerRef.current.getCurrentColor();
+    onValueChange(hexColor);
   }
 
   return (
     <View style={[styles.container, props.containerStyle]}>
       <Label label={props.label} required={props.required} disabled={props.disabled} />
-      <Dialog visible={show} onClose={hideColorPicker}>
-        <View style={{flex: 1, backgroundColor: '#212021'}}>
-          <ColorPicker style={{ flex: 1 }} oldColor='purple' color={color} onColorSelected={onValueChange} />
-        </View>
+      <Dialog visible={show} onClose={hideColorPicker} buttons={[
+            <Button title={props.okText} onPress={selectColor} />
+          ]}>
+        <HsvColorPicker ref={colorPickerRef}
+          huePickerHue={hue}
+          onHuePickerDragEnd={onHuePickerChange}
+          onHuePickerPress={onHuePickerChange}
+          satValPickerHue={hue}
+          satValPickerSaturation={sat}
+          satValPickerValue={val}
+          onSatValPickerDragEnd={onSatValPickerChange}
+          onSatValPickerPress={onSatValPickerChange}
+        />
       </Dialog>
       <TouchableOpacity activeOpacity={Styles.activeOpacity} onPress={showColorPicker}>
         <View style={[styles.colorDot, { backgroundColor: value }, getTextInputStyle()]}></View>
