f4c42bbb01feaff2a6768be42b18c45d08f5317d
Dániel Sipos <siposdani87@hotmail.com>
2020-11-05 19:08:15 +0100

    Fixed autoComplete props

--- src/components/EmailField.tsx
+++ src/components/EmailField.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { TextInputProps } from 'react-native';
 import TextField from './TextField';
 
-export default function EmailField(props: { value: any, onValueChange: (value: any) => void, label?: string, error?: any, required?: boolean, disabled?: boolean, containerStyle?: any, style?: any, children?: any }) {
+export default function EmailField(props: { value: any, onValueChange: (value: any) => void, label?: string, error?: any, required?: boolean, disabled?: boolean, containerStyle?: any, style?: any, children?: any } & TextInputProps) {
   return (
-    <TextField value={props.value} error={props.error} onValueChange={props.onValueChange} label={props.label} required={props.required} disabled={props.disabled} containerStyle={props.containerStyle} style={props.style} autoCapitalize='none' keyboardType='email-address'>
+    <TextField value={props.value} error={props.error} onValueChange={props.onValueChange} label={props.label} required={props.required} disabled={props.disabled} containerStyle={props.containerStyle} style={props.style} autoCapitalize='none' keyboardType='email-address' autoCompleteType={props.autoCompleteType} textContentType={props.textContentType}>
       {props.children}
     </TextField>
   );
