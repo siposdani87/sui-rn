519c8a7c689d42a2599bf3503cc63679bd49cae4
Dániel Sipos <dsipos@szintezis-net.hu>
2021-01-19 12:40:13 +0100

    Fixed return type of cache methods

--- src/utils/Cache.ts
+++ src/utils/Cache.ts
@@ -2,7 +2,7 @@ import { Image } from 'react-native';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 
-export function cacheImages(images): (Promise<Asset> | Promise<boolean>)[] {
+export function cacheImages(images) {
     return images.map((image) => {
         if (typeof image === 'string') {
             return Image.prefetch(image);
@@ -11,7 +11,7 @@ export function cacheImages(images): (Promise<Asset> | Promise<boolean>)[] {
     });
 }
 
-export function cacheFonts(fonts): Promise<void>[] {
+export function cacheFonts(fonts) {
     return fonts.map((font) => {
         return Font.loadAsync(font);
     });
