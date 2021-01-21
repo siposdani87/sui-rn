2634529b75c8e852bb8a9a2bef41e39dced3f859
Dániel Sipos <dsipos@szintezis-net.hu>
2021-01-21 17:37:36 +0100

    Add favicons to project

--- src/screens/OthersScreen.tsx
+++ src/screens/OthersScreen.tsx
@@ -2,8 +2,12 @@ import React, { Fragment } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import { Link, Text, NoContent } from '../../src/components';
 import { StatusBar } from 'expo-status-bar';
+import { RichTextViewer } from 'expo-rich-text-editor/src';
+import { Colors } from '../constants';
 
 export default function OthersScreen() {
+  const sampleHtml = '<p><i><u>Underline italic text</u></i> <b>bold word</b> normal text with some characters <i>Italic word</i> another normal text <u>underline word</u> and email link <a href="mailto:siposdani87@gmail.com">mailto</a> and standar link <a href="https://google.com" target="_blank">link to website</a> and link to <a href="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" target="_blank">download file</a>.</p>';
+
   return (
     <Fragment>
       <StatusBar style='dark' />
@@ -14,6 +18,8 @@ export default function OthersScreen() {
           <Text>Simple themed text</Text>
 
           <Link onPress={() => null} title='Open new link' />
+
+          <RichTextViewer html={sampleHtml} linkStyle={styles.linkStyle} />
         </View>
       </ScrollView>
     </Fragment>
@@ -23,5 +29,8 @@ export default function OthersScreen() {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+  },
+  linkStyle: {
+    color: Colors.primary,
   },
 });
