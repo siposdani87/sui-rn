e82bf07872dccfbb132123461187fed898ec260e
Dániel Sipos <dsipos@szintezis-net.hu>
2021-01-30 10:45:30 +0100

    Add index file to hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useActionColor } from './useActionColor';
+export { default as useDarkTheme } from './useDarkTheme';
+export { default as useErrorField } from './useErrorField';
+export { default as useInputStyle } from './useInputStyle';
