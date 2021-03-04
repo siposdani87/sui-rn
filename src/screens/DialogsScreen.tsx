2e3457421e5cd997944ddefd76316c3898d55b15
Dániel Sipos <dsipos@szintezis-net.hu>
2021-03-04 19:05:22 +0100

    Improved test screens

--- src/screens/DialogsScreen.tsx
+++ src/screens/DialogsScreen.tsx
@@ -26,26 +26,26 @@ export default function DialogsScreen() {
       <ScrollView>
         <View style={styles.container}>
           <Dialog visible={isVisible('dialog1')} onClose={closeDialog}>
-            <Text>Dialog 1 text</Text>
+            <Text>Dialog 1 text this paragraph has long long text to test multiline content and numbers: 1, 34556, 54.54</Text>
           </Dialog>
-          <Button onPress={openDialog('dialog1')} title='dialog1' />
+          <Button onPress={openDialog('dialog1')} title='dialog1 with text' />
 
-          <Dialog visible={isVisible('dialog2')} title='Dialog 2' onClose={closeDialog} buttons={[
+          <Dialog visible={isVisible('dialog2')} title='Dialog 2' buttons={[
             <TextButton key={0} title='cancel' onPress={closeDialog} />,
             <Button key={1} title='done' onPress={closeDialog} />,
           ]}>
             <Text>Dialog 2 text</Text>
             <TextField label='Name' value='' onValueChange={() => null} desc='Description of name' />
           </Dialog>
-          <Button onPress={openDialog('dialog2')} title='dialog2' />
+          <Button onPress={openDialog('dialog2')} title='dialog2 with form' />
 
-          <Dialog visible={isVisible('dialog3')} title='Dialog 3' onClose={closeDialog} buttons={[
+          <Dialog visible={isVisible('dialog3')} title='Dialog 3 with long title and long long!!!' onClose={closeDialog} buttons={[
             <TextButton key={0} title='cancel long title!!!' onPress={closeDialog} />,
             <Button key={1} title='done with long title!!!' onPress={closeDialog} />,
           ]}>
             <Text>Dialog 3 text</Text>
           </Dialog>
-          <Button onPress={openDialog('dialog3')} title='dialog3' />
+          <Button onPress={openDialog('dialog3')} title='dialog3 with long titles' />
 
         </View>
       </ScrollView>
