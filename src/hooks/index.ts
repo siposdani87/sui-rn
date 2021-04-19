0fd41441fd8be290cbbb684a72a2e11147b655e4
Dániel Sipos <dsipos@szintezis-net.hu>
2021-04-19 12:04:02 +0200

    Create useModalState function

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export { default as useActionColor } from './useActionColor';
 export { default as useDarkTheme } from './useDarkTheme';
 export { default as useErrorField } from './useErrorField';
 export { default as useInputStyle } from './useInputStyle';
+export { default as useModalState } from './useModalState';
+
---
+++ src/hooks/useModalState.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function useModalState(initialOpen?: boolean): [boolean, () => void, () => void, () => void] {
+    const [visible, setVisible] = useState<boolean>(initialOpen ?? false);
+
+    const open = () => {
+        setVisible(true);
+    };
+
+    const close = () => {
+        setVisible(false);
+    };
+
+    const toggle = () => {
+        setVisible(!visible);
+    };
+
+    return [visible, open, close, toggle];
+}
