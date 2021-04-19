e392eb62c019ff4742b4e64d267015774be58846
Dániel Sipos <siposdani87@hotmail.com>
2021-04-19 12:04:02 +0200

    Create useModalState function

--- src/screens/DialogsScreen.tsx
+++ src/screens/DialogsScreen.tsx
@@ -1,51 +1,40 @@
-import React, { Fragment, useState } from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
-import { Dialog, Button, Text, TextButton, TextField } from '../../src/components';
 import { StatusBar } from 'expo-status-bar';
+import React, { Fragment } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Button, Dialog, Text, TextButton, TextField } from '../../src/components';
+import { useModalState } from '../hooks';
 
 export default function DialogsScreen() {
-  const [visible, setVisible] = useState(null);
-
-  function isVisible(key: string): boolean {
-    return visible === key;
-  }
-
-  function openDialog(key: string): any {
-    return () => {
-      setVisible(key);
-    };
-  }
-
-  function closeDialog(): any {
-    setVisible(null);
-  }
+  const [dialog1, openDialog1, closeDialog1] = useModalState();
+  const [dialog2, openDialog2, closeDialog2] = useModalState();
+  const [dialog3, openDialog3, closeDialog3] = useModalState();
 
   return (
     <Fragment>
       <StatusBar style='dark' />
       <ScrollView>
         <View style={styles.container}>
-          <Dialog visible={isVisible('dialog1')} onClose={closeDialog}>
+          <Dialog visible={dialog1} onClose={closeDialog1}>
             <Text>Dialog 1 text this paragraph has long long text to test multiline content and numbers: 1, 34556, 54.54</Text>
           </Dialog>
-          <Button onPress={openDialog('dialog1')} title='dialog1 with text' />
+          <Button onPress={openDialog1} title='dialog1 with text' />
 
-          <Dialog visible={isVisible('dialog2')} title='Dialog 2' buttons={[
-            <TextButton key={0} title='cancel' onPress={closeDialog} />,
-            <Button key={1} title='done' onPress={closeDialog} />,
+          <Dialog visible={dialog2} title='Dialog 2' buttons={[
+            <TextButton key={0} title='cancel' onPress={closeDialog2} />,
+            <Button key={1} title='done' onPress={closeDialog2} />,
           ]}>
             <Text>Dialog 2 text</Text>
             <TextField label='Name' value='' onValueChange={() => null} desc='Description of name' />
           </Dialog>
-          <Button onPress={openDialog('dialog2')} title='dialog2 with form' />
+          <Button onPress={openDialog2} title='dialog2 with form' />
 
-          <Dialog visible={isVisible('dialog3')} title='Dialog 3 with long title and long long!!!' onClose={closeDialog} buttons={[
-            <TextButton key={0} title='cancel long title!!!' onPress={closeDialog} />,
-            <Button key={1} title='done with long title!!!' onPress={closeDialog} />,
+          <Dialog visible={dialog3} title='Dialog 3 with long title and long long!!!' onClose={closeDialog3} buttons={[
+            <TextButton key={0} title='cancel long title!!!' onPress={closeDialog3} />,
+            <Button key={1} title='done with long title!!!' onPress={closeDialog3} />,
           ]}>
             <Text>Dialog 3 text</Text>
           </Dialog>
-          <Button onPress={openDialog('dialog3')} title='dialog3 with long titles' />
+          <Button onPress={openDialog3} title='dialog3 with long titles' />
 
         </View>
       </ScrollView>
