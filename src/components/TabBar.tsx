df07f6afaacf046844196c7eddfe19687552eb13
Dániel Sipos <dsipos@szintezis-net.hu>
2021-08-22 22:44:25 +0200

    Fixed style of TabBar component

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,20 +1,20 @@
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Colors, Styles } from '../constants';
 
-export default function TabBar(props: BottomTabBarProps & { hasPaddingBottom?: boolean, tabBarStyle?: any, tabBarItemStyle?: any, tabBarLabelStyle?: any, tabBarActiveTintColor?: string, tabBarInactiveTintColor?: string }) {
-  const insets = useSafeAreaInsets();
-
+export default function TabBar(props: BottomTabBarProps & { hasPaddingBottom?: boolean }) {
   const { descriptors, state, navigation } = props;
 
   if (state.routes.length < 2) {
     return null;
   }
 
+  const routeKey = state.routes[state.index].key;
+  const tabBarOptions = descriptors[routeKey].options as any;
+
   return (
-    <View style={[props.tabBarStyle, { flexDirection: 'row', paddingBottom: props.hasPaddingBottom ? 0 : insets.bottom }]}>
+    <View style={[tabBarOptions.tabBarStyle, { flexDirection: 'row', paddingBottom: props.hasPaddingBottom ? 0 : props.insets.bottom }]}>
       {state.routes.map((route, index) => {
         const { options } = descriptors[route.key];
         const label = options.tabBarLabel ? options.tabBarLabel : options.title ? options.title : route.name;
@@ -41,9 +41,9 @@ export default function TabBar(props: BottomTabBarProps & { hasPaddingBottom?: b
 
         return (
           <TouchableOpacity key={index} activeOpacity={Styles.activeOpacity} accessibilityRole='button' accessibilityLabel={options.tabBarAccessibilityLabel} testID={options.tabBarTestID} onPress={onPress} onLongPress={onLongPress} style={{ flex: 1 }}>
-            <View style={[props.tabBarItemStyle, styles.tabContainer, { borderTopColor: isFocused ? Colors.accent : 'transparent' }]}>
-              <Text numberOfLines={2} style={[props.tabBarLabelStyle, styles.tabText, {
-                color: isFocused ? props.tabBarActiveTintColor : props.tabBarInactiveTintColor,
+            <View style={[tabBarOptions.tabBarItemStyle, styles.tabContainer, { borderTopColor: isFocused ? Colors.accent : 'transparent' }]}>
+              <Text numberOfLines={2} style={[tabBarOptions.tabBarLabelStyle, styles.tabText, {
+                color: isFocused ? tabBarOptions.tabBarActiveTintColor : tabBarOptions.tabBarInactiveTintColor,
                 fontFamily: isFocused ? Styles.fontFamilyBodyMedium : Styles.fontFamilyBodyRegular,
                 fontWeight: isFocused ? '500' : '400',
               }]}>
