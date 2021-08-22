6e7e923ecc20b540dd6b40d38308dcf0a4926375
Dániel Sipos <siposdani87@hotmail.com>
2021-08-22 14:12:07 +0200

    Improved TabBar component

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 sudo npm i -g eslint expo-cli expo-optimize
 rm -rf node_modules/sui-js
 rm -rf node_modules/expo-rich-text-editor
+rm -rf node_modules/expo-maps-polygon-editor
 rm -rf package-lock.json
 npm install
 npm outdated
