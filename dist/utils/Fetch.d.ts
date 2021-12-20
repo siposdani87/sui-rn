0e570bfbc703eaeb2a29fd02f26a84d138ddf2ee
Dániel Sipos <dsipos@szintezis-net.hu>
2021-12-20 18:35:43 +0100

    Fixed Response types

--- dist/utils/Fetch.d.ts
+++ dist/utils/Fetch.d.ts
@@ -7,6 +7,9 @@ export interface Params {
 export interface Headers {
     [name: string]: string;
 }
+export interface HttpResponse extends Response {
+    data: any;
+}
 interface MimeTypes {
     [key: string]: string;
 }
@@ -15,11 +18,11 @@ export default class Fetch {
     constructor(backendUrl: string);
     get mimeTypes(): MimeTypes;
     getUrl(url: string, opt_params?: Params): string;
-    get(url: string, opt_params?: Params, opt_headers?: Headers): Promise<any>;
-    post(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<any>;
-    put(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<any>;
-    patch(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<any>;
-    delete(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<any>;
+    get(url: string, opt_params?: Params, opt_headers?: Headers): Promise<HttpResponse>;
+    post(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<HttpResponse>;
+    put(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<HttpResponse>;
+    patch(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<HttpResponse>;
+    delete(url: string, opt_data?: Data, opt_params?: Params, opt_headers?: Headers): Promise<HttpResponse>;
     private _getHeaders;
     private filteredHeaders;
     private _getQuery;
