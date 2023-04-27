8679dc1c56f83b0c063a82e330d622673714b44d
Dániel Sipos <siposdani87@hotmail.com>
2023-04-27 19:13:28 +0200

    Set size of dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -93,6 +93,8 @@ const styles = StyleSheet.create({
         alignItems: 'stretch',
         borderRadius: 3,
         margin: 20,
+        maxWidth: 520,
+        minWidth: 360,
     },
     success: {
         borderTopWidth: 5,
